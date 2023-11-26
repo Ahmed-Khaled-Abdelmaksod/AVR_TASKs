ad1b5d9f714f3eef788693c85aa585faf759488c
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2023-11-26 19:15:23 +0200

    Add files via upload

---
+++ THREE LEDS BLINKS/application/Debug/application_program.d
@@ -0,0 +1,15 @@
+application_program.o application_program.o: ../application_program.c \
+  ../HAL/LED/LED_interface.h ../HAL/LED/../../MCAL/DIO/DIO_interface.h \
+  ../HAL/LED/../../MCAL/DIO/DIO_private.h \
+  ../HAL/LED/../../MCAL/DIO/../../Libraries/STD_TYPES.h \
+  ../HAL/LED/../../MCAL/DIO/../../Libraries/BIT_MATH.h
+
+../HAL/LED/LED_interface.h:
+
+../HAL/LED/../../MCAL/DIO/DIO_interface.h:
+
+../HAL/LED/../../MCAL/DIO/DIO_private.h:
+
+../HAL/LED/../../MCAL/DIO/../../Libraries/STD_TYPES.h:
+
+../HAL/LED/../../MCAL/DIO/../../Libraries/BIT_MATH.h:
